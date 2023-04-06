e5b18db1e91b1566a178866024727ea10c1c1501
Alexander Park <111792124+Alexxpark@users.noreply.github.com>
2023-04-05 22:38:33 -0500

    Added animations for the About me Button.

---
+++ script.js
@@ -0,0 +1,3 @@
+<button class="about-button" onclick="toggleAbout()">About Me</button>
+					<p id="about">A brief paragraph about you and what kind of job/company you are looking to work for.</p>
+
